3c9467bc4efd4342688e2d3f9f985e91f87bdc71
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-02-11 20:23:01 -0600

    Add run script

---
+++ python/run.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# REQ: Runs the python script. <skr 2023-02-11>
+
+set +o bracexpand
+set -o errexit
+set -o noclobber
+set -o noglob
+set -o nounset
+set -o pipefail
+set -o xtrace 
+
+script_dir=$(dirname "$0")
+
+pip install -r "$script_dir/requirements.txt"
+
+"$script_dir/main.py"
